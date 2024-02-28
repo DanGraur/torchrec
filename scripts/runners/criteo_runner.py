45b4e2dffe3ce0e24bd7d9db0b428099479146bd
DanGraur <dan.graur@inf.ethz.ch>
2024-02-28 17:41:16 +0100

    Added automation to the Criteo experiments.

--- scripts/runners/criteo_runner.py
+++ scripts/runners/criteo_runner.py
@@ -1,3 +1,6 @@
+from absl import app
+from absl import flags
+
 import time
 
 from torch.utils.data import DataLoader
@@ -5,34 +8,42 @@ import torch.utils.data.datapipes as dp
 
 from torchrec.datasets.criteo import criteo_terabyte
 
-WORKER_COUNT = 94
-BATCH_SIZE = 32
-EXPERIMENT_TIME = 120  # should be in seconds
-FILES = ['/home/dan/projects/db-preproc/data/train.tsv'] * 13
+FLAGS = flags.FLAGS
 
-# Set up the input pipeline
-df = criteo_terabyte(FILES)
-df = dp.iter.Batcher(df, BATCH_SIZE)
-df = dp.iter.Collator(df)
-dataset = DataLoader(df, num_workers=WORKER_COUNT)
+flags.DEFINE_integer('batch_size', 512, 'The batch size.')
+flags.DEFINE_integer('experiment_time', 120, 'The total experiment time.')
+flags.DEFINE_integer('worker_count', 32, 'The total number of parallel workers.')
 
-batch_iterator = iter(dataset)
+def main(argv):
+  del argv
+  FILES = ['/home/dan/projects/db-preproc/data/train.tsv'] * FLAGS.worker_count
 
-batch_idx = 0
-s = time.time()
-while time.time() - s <= EXPERIMENT_TIME:
-  batch = next(batch_iterator)
-  if batch_idx % 1000 == 0:
-    print(f"At batch #{batch_idx}")
-  batch_idx += 1
-s = time.time() - s
+  df = criteo_terabyte(FILES)
+  df = dp.iter.Batcher(df, FLAGS.batch_size)
+  df = dp.iter.Collator(df)
+  dataset = DataLoader(df, num_workers=FLAGS.worker_count)
 
-# for batch_idx, batch in enumerate(batch_iterator):
-#   if time.time() - s >= EXPERIMENT_TIME:
-#     break
-#   if batch_idx % 1000 == 0:
-#     print(f"At batch #{batch_idx}")
-# s = time.time() - s
+  batch_iterator = iter(dataset)
 
-print(f"Completed the experiment; {batch_idx * BATCH_SIZE / s} "
-      f"[instances/s] = ({batch_idx} * {BATCH_SIZE}) / {s}")
+  batch_idx = 0
+  s = time.time()
+  while time.time() - s <= FLAGS.experiment_time:
+    batch = next(batch_iterator)
+    if batch_idx % 1000 == 0:
+      print(f"At batch #{batch_idx}")
+    batch_idx += 1
+  s = time.time() - s
+
+  # for batch_idx, batch in enumerate(batch_iterator):
+  #   if time.time() - s >= EXPERIMENT_TIME:
+  #     break
+  #   if batch_idx % 1000 == 0:
+  #     print(f"At batch #{batch_idx}")
+  # s = time.time() - s
+
+  print(f"Instance Throughput {batch_idx * FLAGS.batch_size / s} "
+        f"[instances/s] = ({batch_idx} * {FLAGS.batch_size}) / {s}")
+
+
+if __name__ == '__main__':
+  app.run(main)
---
+++ scripts/runners/run_batch_experiment.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+experiment=${1:-"criteo"}
+worker_count=${2:-"32"}
+experiment_time=${3:"120"}
+
+# Prepare parameters
+batch_sizes="$( for i in {4..14}; do echo $((2**$i)); done)"
+
+# Prepare target experiment documents
+EXPERIMENT_FOLDER="experiments/torchrec_${experiment}_$(date +%F_%H_%M_%S_%3N)"
+RESULT_FILE="${EXPERIMENT_FOLDER}/results.csv"
+mkdir -p ${EXPERIMENT_FOLDER}
+command="${experiment}_runner.py"
+params=""
+if [ "${experiment}" == "criteo" ]; then
+  params="--batch_size=${i} --worker_count=${worker_count} --experiment_time=${experiment_time}"
+fi
+echo "system,sample_count,batch,throughput" > ${RESULT_FILE}
+
+# Run experiment
+for i in ${batch_sizes}; do
+  echo "Starting experiment for ${i} batch size..."
+  experiment_log="${EXPERIMENT_FOLDER}/batch_${i}.log"
+  python3 ${command} ${params} > >(tee ${experiment_log}) 2> /dev/null
+  throughput=$( cat ${experiment_log} | tail -n 1 | awk '{print $3}' )
+  echo "pytorch,10000000,${i},${throughput}" >> ${RESULT_FILE}
+  echo "Finished experiment for ${i} batch size!"
+done
+
+echo "Results now available in: ${RESULT_FILE}"
