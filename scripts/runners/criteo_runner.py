ce4b615d0122ca5f879fba5791f6eb5f5ab3d228
DanGraur <dan.graur@inf.ethz.ch>
2024-02-28 10:45:51 +0100

    Added dataloader support for Criteo input pipeline

--- scripts/runners/criteo_runner.py
+++ scripts/runners/criteo_runner.py
@@ -1,19 +1,22 @@
 import time
 
+from torch.utils.data import DataLoader
 import torch.utils.data.datapipes as dp
 
 from torchrec.datasets.criteo import criteo_terabyte
 
+WORKER_COUNT=94
 BATCH_SIZE = 32
 MAX_BATCH_COUNT = 1000
-FILES = ['/home/dan/projects/db-preproc/data'] * 13
+FILES = ['/home/dan/projects/db-preproc/data/train.tsv'] * 13
 
 # Set up the input pipeline
 df = criteo_terabyte(FILES)
 df = dp.iter.Batcher(df, BATCH_SIZE)
 df = dp.iter.Collator(df)
+dataset = DataLoader(df, num_workers=WORKER_COUNT)
 
-batch_iterator = iter(df)
+batch_iterator = iter(dataset)
 
 s = time.time()
 for batch_idx, batch in enumerate(batch_iterator):
@@ -24,4 +27,4 @@ for batch_idx, batch in enumerate(batch_iterator):
 s = time.time() - s
 
 print(f"Completed the experiment; {MAX_BATCH_COUNT * BATCH_SIZE / s} "
-      f"[instances/s] = {MAX_BATCH_COUNT} * {BATCH_SIZE}) / {s}")
+      f"[instances/s] = ({MAX_BATCH_COUNT} * {BATCH_SIZE}) / {s}")
